66aa2365b6812b0f48dbd4d6cc6f85be1417b28a
egor-turaev <egor-turaev@mail.ru>
2022-08-14 22:12:13 +0300

    1save

---
+++ 19/Program.cs
@@ -0,0 +1,17 @@
+﻿// Console.WriteLine("Введите пятзначное число: ");
+// string str  = Console.ReadLine();
+// int number = int.Parse(numberString!);
+// if (number > 9999 && number < 100000)
+
+Console.WriteLine("Введите пятизначное число: ");
+
+string number  = Console.ReadLine();
+
+if (number[0] == number[4] && number[1] == number[3] )
+
+    Console.WriteLine("Да");
+
+else
+{
+    Console.WriteLine("Нет");
+}
