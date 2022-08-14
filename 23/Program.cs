e39804f43146f77acdf582899fc1823819f56f18
egor-turaev <egor-turaev@mail.ru>
2022-08-14 22:12:37 +0300

    1save

---
+++ 23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+string? numberString = Console.ReadLine();
+int number = int.Parse(numberString!);
+int i = 0;
+while(i < number+1)
+{
+    Console.WriteLine(i*i*i);
+    i++;
+}
